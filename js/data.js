b9aad11373dc51adf6d03cd093e8459c00e07432
QueroBurguer <virtualservicegames@gmail.com>
2026-02-21 15:38:46 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,23 +1,14 @@
 const PRODUCTS = [
     { id: 'qbsmash', name: 'Quero Smash', price: 18.90, category: 'Artesanal', desc: 'Um delicioso Smash Burguer com a carne suculenta por dentro e aquela crostinha por fora, Cheddar e Cebola Caramelizada!', image: 'images/QueroSmashBurguer.jpg' },
+   
     { id: 'combofamilia4', name: 'Combo Família 4', price: 69.90, category: 'Combos', desc: '4 Deliciosos X-Tudos + Batata Turbinada 600g + Bebida Gelada 2L.', image: 'images/combof2.jpeg' },
     { id: 'combofamilia3', name: 'Combo Família 3', price: 59.90, category: 'Combos', desc: '3 Deliciosos X-Tudos + Batata Turbinada 600g + Bebida Gelada 2L.', image: 'images/combof1.jpeg' },
     { id: 'comboxtudo', name: 'Combo X-Tudo', price: 18.90, category: 'Combos', desc: 'X-Tudo Delicioso + Batata Frita Crocrante 140g + Bebida Gelada', image: 'images/QueroComboXTudo.jpeg' },
-    { id: 'batatam', name: 'Batata Frita Média', price: 18.90, category: 'Acompanhamentos', desc: 'Batata Crocante.', image: 'images/batatam.jpeg' },
+
+    { id: 'batatafritaP', name: 'Batata Frita Crocrante 140g', price: 7.90, category: 'Batatas Fritas', desc: 'Batata Frita na hora, Crocrante por fora e Macia por dentro 140g.', image: 'images/QueroBatataFrita.jpeg' },
     { id: 'batatag', name: 'Batata Frita Grande', price: 24.90, category: 'Acompanhamentos', desc: 'Batata Crocante.', image: 'images/batatag.jpeg' },
-    { id: 'guaramor', name: 'Guaramor 250ml', price: 4.90, category: 'Bebidas', desc: 'Guaraná 250ml.', image: 'images/guaramor.jpg' },
-    { id: 'batatamtb', name: 'Batata Turbinada Média', price: 23.90, category: 'Acompanhamentos', desc: 'Batata frita com cheddar e bacon.', image: 'images/batatag1.jpeg' },
-    { id: 'batatagtb', name: 'Batata Turbinada Grande', price: 27.90, category: 'Acompanhamentos', desc: 'Batata frita com cheddar e bacon.', image: 'images/batatag2.jpeg' },
-    { id: 'xbacon', name: 'Quero Bacon', price: 18.90, category: 'Lanches', desc: 'Carne 160g, bacon crocante, cheddar e molho especial.', image: 'images/bacon.jpg' },
-    { id: 'xtudo', name: 'Quero X-tudo', price: 18.90, category: 'Lanches', desc: 'Carne 160g, bacon crocante, cheddar e molho especial.', image: 'images/Xtudo.jpg' },
-    { id: 'qbpredileto', name: 'Quero Burguer Predileto', price: 18.90, category: 'Lanches', desc: 'Um burger artesanal de 120g super suculento, coberto com cheddar derretido e muita cebola caramelizada no ponto perfeito. Ganha crocância com o bacon e frescor com o tomate e os picles.', image: 'images/bacon.jpg' },
-    { id: 'qbxtudo', name: 'Quero X-Tudo', price: 18.90, category: 'Lanches', desc: 'Aquele X-Tudo raiz que todo mundo ama! Pão macio, hambúrguer tradicional bem fritinho, queijo derretido, presunto saboroso e um ovo fritinho. Completo com alface, cebola roxa e maionese temperada.', image: 'images/Xtudo.jpg' },
-    { id: 'qbsmashduplo', name: 'Quero Smash Duplo 200g', price: 18.90, category: 'Lanches', desc: 'Dois discos de carne smash crocantes por fora e suculentos por dentro, cada um coberto com queijo derretido. Finalizado com bacon crocante e baconese.', image: 'images/bacon.jpg' },
-    { id: 'qbxsalada', name: 'Quero X-Salada', price: 18.90, category: 'Lanches', desc: 'Sabor e frescor em cada mordida! Pão macio, carne suculenta, tomate e alface fresquinhos cuidadosamente selecionados.', image: 'images/bacon.jpg' },
-    { id: 'qbkids', name: 'Quero Hambúrguer Kids', price: 18.90, category: 'Lanches', desc: 'Pequeno no tamanho, gigante no sabor! Pão macio, carne suculenta e queijo derretido, perfeito para a criançada.', image: 'images/bacon.jpg' },
-    { id: 'qbespecial', name: 'Quero Burguer Especial', price: 27.90, category: 'Lanches', desc: 'Burger artesanal de 120g, suculento e bem temperado, coberto com queijo cheddar cremoso. Recebe o frescor do tomate, da alface crocante e da cebola roxa.', image: 'images/.jpeg' },
-    { id: 'qbduplo', name: 'Quero Hamburguer Duplo', price: 11.90, category: 'Lanches', desc: 'Duplo sabor, dupla intensidade! Pão macio, duas camadas suculentas de carne, queijo derretido e bacon crocante.', image: 'images/smash.jpg' },
-];
+   
+   ];
 
 const DRINK_OPTIONS = [
     { id: 'coca_lata', name: 'Coca-Cola Lata 350ml', price: 6.00 },
@@ -45,4 +36,5 @@ const SHIPPING = {
     pricePerKm: 1.50, // Adicional por km
     city: 'São Gonçalo' // Cidade padrão para busca
 };
+
 
